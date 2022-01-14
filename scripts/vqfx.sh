2451cea1ddef9480d6198ef364892d626c705412
omaxx <maxx.orlov@gmail.com>
2022-01-14 17:18:53 -0500

    add junos/vqfx and junos/vsrx

--- scripts/vqfx.sh
+++ scripts/vqfx.sh
@@ -1,18 +1,24 @@
-virt-install --name vQFX1-RE \
-             --ram 1024 \
-             --cpu Haswell,+vmx \
-             --vcpus=1 \
-             --arch=x86_64 \
-             --disk path=/var/lib/libvirt/images/vqfx10k-1-re.qcow2,size=16,device=disk,bus=ide,format=qcow2 \
-             --import \
-             --network=network:default,model=e1000 --network=network:internal1,model=e1000 --network=network:reserved,model=e1000 \
-             --network=network:vmnet1,model=e1000 --network=network:vmnet3,model=e1000 --network=network:vmnet4,model=e1000
+virt-install \
+    --name r1-re \
+    --ram 1024 \
+    --vcpus=1 \
+    --arch=x86_64 \
+    --disk path=/home/maxim/vlab/r1/re/vqfx-20.2R1.10-re-qemu.qcow2 \
+    --import \
+    --network=network:mgmt,model=e1000 \
+    --network=network:r1-int,model=e1000 \
+    --network=network:r1-res,model=e1000 \
+    --network=network:r1_r2_1,model=e1000 \
+    --network=network:r1_r2_2,model=e1000 \
+    &
 
-virt-install --name vQFX1-PFE \
-             --ram 2048 \
-             --cpu Haswell,+vmx \
-             --vcpus=1 \
-             --arch=x86_64 \
-             --disk path=/var/lib/libvirt/images/vqfx10k-1-pfe.qcow2,device=disk,bus=ide,format=qcow2 \
-             --import \
-             --network=network:default,model=e1000 --network=network:internal1,model=e1000
+virt-install \
+    --name r1-pfe \
+    --ram 2048 \
+    --vcpus=1 \
+    --arch=x86_64 \
+    --disk path=/home/maxim/vlab/r1/pfe/vqfx-20.2R1-2019010209-pfe-qemu.qcow \
+    --import \
+    --network=network:mgmt,model=e1000 \
+    --network=network:r1-int,model=e1000 \
+    &
--- scripts/vsrx.sh
+++ scripts/vsrx.sh
@@ -9,3 +9,12 @@ virt-install --name H11 \
              --network=network:default,model=virtio \
              --network=network:default,model=virtio &
 
+virt-install --name SRX \
+            --ram 4096 \
+            --vcpus=2 \
+            --arch=x86_64 \
+            --disk path=/var/lib/libvirt/images/SRX/media-vsrx-vmdisk-15.1X49-D150.2.qcow \
+            --import \
+            --network=network:default,model=virtio \
+            --network=network:default,model=virtio \
+            --network=network:default,model=virtio &
